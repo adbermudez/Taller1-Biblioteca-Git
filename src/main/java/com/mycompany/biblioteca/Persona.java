277c66edc484a4235abad8baa4c5f29aff0fe6e4
Angel Bermudez <adbermudez@unicesar.edu.co>
2026-08-19 07:11:45 -0500

    feat: crear clase Material como padre de Libro

--- src/main/java/com/mycompany/biblioteca/Persona.java
+++ src/main/java/com/mycompany/biblioteca/Persona.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author ASUS
- */
 public class Persona {
+    private String id;
+    private String name;
+    private String phoneNumb;
+
+    public Persona(String id, String name, String phoneNumb) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumb = phoneNumb;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumb() {
+        return phoneNumb;
+    }
+
+    public void setPhoneNumb(String phoneNumb) {
+        this.phoneNumb = phoneNumb;
+    }
     
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Nombre: " + name + " | Teléfono: " + phoneNumb;
+    }
 }
