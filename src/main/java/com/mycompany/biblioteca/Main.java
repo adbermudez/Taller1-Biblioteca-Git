8da12e01efa6e9057b3cff3a905aaa0da79cc91a
Angel Bermudez <adbermudez@unicesar.edu.co>
2026-08-19 07:04:59 -0500

    fix: restaurar contenido de Persona y Cliente

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -2,6 +2,7 @@
 package com.mycompany.biblioteca;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
