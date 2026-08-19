7a1fbb02b81bf2f8ed880f6e79b7dab45160b8e5
Angel Bermudez <adbermudez@unicesar.edu.co>
2026-08-18 21:56:59 -0500

    feat: implementar CREATE de Cliente

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -1,16 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author ASUS
- */
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class Main {
+    static ArrayList<Cliente> clientes = new ArrayList<>();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+            
+    }
+    
+    static void crearCliente() throws IOException {
+        System.out.print("ID: ");
+        String id = br.readLine();
+        System.out.print("Nombre: ");
+        String name = br.readLine();
+        System.out.print("Telefono: ");
+        String phoneNumb = br.readLine();
+        System.out.print("Email: ");
+        String email = br.readLine();
+        
+        Cliente c = new Cliente(id, name, phoneNumb, email);
+        clientes.add(c);
+        System.out.println("Cliente creado correctamente. ");
     }
 }
