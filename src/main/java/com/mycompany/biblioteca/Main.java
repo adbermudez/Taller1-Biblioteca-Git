e08a821d3805cc2b24edf6091f262399d2f09675
Angel Bermudez <adbermudez@unicesar.edu.co>
2026-08-18 22:55:00 -0500

    feat: implementar UPDATE de Cliente

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -4,6 +4,7 @@ package com.mycompany.biblioteca;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main {
     static ArrayList<Cliente> clientes = new ArrayList<>();
@@ -45,5 +46,29 @@ public class Main {
             }
         }
         return null;
+    }
+    
+    static void actualizarCliente() throws IOException {
+        
+        System.out.print("ID del cliente a actualizar: ");
+        String id = br.readLine();
+        Cliente c = buscarCliente(id);
+        
+        if (c == null){
+            System.out.println("Cliente no encontrado.");
+            return;
+        }
+        System.out.print("Nuevo nombre (" + c.getName() + "):");
+        String name = br.readLine();
+        System.out.print("Nuevo telefono (" + c.getName() + "):");
+        String phoneNumb = br.readLine();
+        System.out.print("Nuevo email (" + c.getEmail() + "): ");
+        String email = br.readLine();
+        
+        c.setName(name);
+        c.setPhoneNumb(phoneNumb);
+        c.setEmail(email);
+        
+        System.out.println("Cliente actualizado correctamente.");
     }
 }
